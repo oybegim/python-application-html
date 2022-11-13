c39b7146af80a5a67c0e96874fbf6f6106fc4eb7
oybegim <sultonovaoybegim@main.com>
2022-11-13 20:05:43 +0300

    main

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 
-from .models import Talaba
+from .models import *
 admin.site.register(Talaba)
+admin.site.register(Pepsi)
--- core/models.py
+++ core/models.py
@@ -3,4 +3,8 @@ from django.db import models
 class Talaba(models.Model):
     ism = models.CharField(max_length=16)
     familiya = models.CharField(max_length=16)
-    
+
+
+class Pepsi(models.Model):
+    narx = models.IntegerField(default=5)
+    litr = models.FloatField(default=0.5)
